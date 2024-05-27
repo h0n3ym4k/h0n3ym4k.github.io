adf29376e9f8fbf0b1e1afc5b26780efdc5d8747
h0n3ym4k <98036119lmak@gmail.com>
2024-05-27 09:56:34 +0800

    prep ti.txt

--- grep_apache.sh
+++ grep_apache.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 zcat /var/log/apache2/access_log*|awk '{print $1}'|sort|uniq -c|sort -h|awk '$1 > 100 {print ;}'
 
+echo
+zcat /var/log/apache2/access_log*|awk '{print $1}'|sort|uniq -c|sort -h|awk '$1 > 100 {print ;}'|echo -n `awk '{print $2}'` > /tmp/web.txt
+echo
+
 for i in $(zcat /var/log/apache2/access_log*|awk '{print $1}'|sort|uniq -c|sort -h|awk '$1 > 100 {print ;}'|awk '{print $2}')
 do
 	echo
--- grep_postfix.sh
+++ grep_postfix.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 zgrep postfix /var/log/messages*|egrep ' connect from'|awk '{print $8}'|egrep -o '[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}'|sort|uniq -c|sort -h|awk '$1 > 100 {print ;}'
 
+zgrep postfix /var/log/messages*|egrep ' connect from'|awk '{print $8}'|egrep -o '[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}'|sort|uniq -c|sort -h|awk '$1 > 100 {print ;}'|awk '{print $2 " REJECT"}' > /tmp/mail.txt
+
 for i in $(zgrep postfix /var/log/messages*|egrep ' connect from'|awk '{print $8}'|egrep -o '[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}'|sort|uniq -c|sort -h|awk '$1 > 100 {print ;}'|awk '{print $2}')
 do
 	echo
