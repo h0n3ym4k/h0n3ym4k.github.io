5e7338e4438031026f93b0d72f69f071124093ef
h0n3ym4k <98036119lmak@gmail.com>
2024-05-30 07:17:07 +0800

    Update world-ing.sh

--- world-ing.sh
+++ world-ing.sh
@@ -2,12 +2,12 @@
 
 cd /root
 
-if [ -e /root/world-ing.stop-cron-weekly ];then
-	echo 'stopping weekly - /root/world-ing.stop-cron-weekly'
+if [ -e /root/world-ing.stop-cron-weekly.enabled ];then
+	echo 'stopping weekly - /root/world-ing.stop-cron-weekly.enabled'
 	exit
 fi
 
 emerge --sync --color n
 echo;echo;echo;echo "---"
-emerge world -Dupv --color n|egrep 'gcc|glibc|sources'
+emerge world -Dupv --color n|egrep 'gcc|glibc|sources' && echo 'compile.sh webalizer'
 emerge world -Dupv --color y|ansi2html > /tmp/weekly.html && mailx -a /tmp/weekly.html -s 'weekly update' 'root@localhost'
