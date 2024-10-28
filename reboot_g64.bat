e9ed95e07bc092ea44d3d19d7d74df0fe06c8213
h0n3ym4k <98036119lmak@gmail.com>
2024-10-28 10:43:14 +0800

    Create reboot_g64.bat

---
+++ reboot_g64.bat
@@ -0,0 +1,32 @@
+@echo off
+
+REM Define the path to the text file
+set "filepath=C:\Users\user\Downloads\request_reboot.txt"
+
+REM Check if the file exists
+if exist "%filepath%" (
+    call :acpipowerbutton
+    timeout /t 10 /nobreak > NUL
+    call :startvm
+    timeout /t 10 /nobreak > NUL
+) else (
+    exit /b
+)
+
+REM End of script
+exit /b
+
+:acpipowerbutton
+echo File exists. Executing acpipowerbutton function...
+REM Add your acpipowerbutton commands here
+"C:\Program Files\Oracle\VirtualBox\VBoxManage.exe" controlvm "g64" acpipowerbutton
+timeout /t 10 /nobreak > NUL
+del "%filepath%"
+REM exit /b
+
+:startvm
+echo File does not exist. Executing startvm function...
+REM Add your startvm commands here
+"C:\Program Files\Oracle\VirtualBox\VBoxManage.exe" startvm "g64"
+timeout /t 10 /nobreak > NUL
+REM exit /b
