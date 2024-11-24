bd447d75715ed92b2fa271786e2aa4005137fe9d
h0n3ym4k <98036119lmak@gmail.com>
2024-11-24 17:09:31 +0800

    Update world-ing.sh

--- world-ing.sh
+++ world-ing.sh
@@ -9,5 +9,10 @@ fi
 
 emerge --sync --color n
 echo;echo;echo;echo "---"
-emerge world -Dupv --color n|egrep 'gcc|glibc|sources|portage' && echo 'compile.sh webalizer'
-emerge world -Dupv --color y|ansi2html > /tmp/weekly.html && mailx -a /tmp/weekly.html -s 'weekly update' 'root@localhost'
+emerge world -Dupv --color n|egrep 'portage' && echo 'compile.sh webalizer'
+echo;echo;echo;echo "---"
+emerge world -Dupv --color n|egrep 'openssl|gcc|glibc|sources' && echo 'compile.sh webalizer'
+emerge world -Dupv --color y &>/tmp/weekly.txt && cat /tmp/weekly.txt|ansi2html > /tmp/weekly.html && mailx -a /tmp/weekly.html -s 'weekly update' 'root@localhost'
+echo;echo;echo;echo "---"
+#try
+#equery l -I \*
