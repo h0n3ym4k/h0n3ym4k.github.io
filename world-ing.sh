6ef8564f8d65ec094b20b17e59f3530aa5828aa0
h0n3ym4k <98036119lmak@gmail.com>
2025-11-17 18:55:21 +0800

    Update world-ing.sh

--- world-ing.sh
+++ world-ing.sh
@@ -18,5 +18,8 @@ echo;echo;echo;echo "---"
 emerge world -Dupv --color n|egrep 'openssl|gcc|glibc|sources' && echo 'compile.sh webalizer' && echo 'rm build log - rebuild world' && head -n 30 build.log.gen.html.sh
 emerge world -Dupv --color y &>/tmp/weekly.txt && cat /tmp/weekly.txt|ansi2html > /tmp/weekly.html && cat /root/gcc-native.txt.html >> /tmp/weekly.html && cat /root/cpuinfo.txt.html >> /tmp/weekly.html && cp -fv /tmp/weekly.html /mnt/Downloads/portage-build-log/weekly.html && mailx -a /tmp/weekly.html -s 'weekly update' 'root@localhost'
 echo;echo;echo;echo "---"
+emerge world -Dupv --color n &>/tmp/weekly.txt.nocolor && cp -v /tmp/weekly.txt.nocolor /mnt/Downloads
+qatom -C `egrep -o '\][[:space:]][[:alnum:]](.*)::gentoo[[:space:]]' /tmp/weekly.txt.nocolor | cut -b1,2 --complement` > /tmp/change.txt && cat /tmp/change.txt |cut -f1,2 -d' '|sed 's/ /\//' >/tmp/qatom-change.txt && cp -v /tmp/qatom-change.txt /mnt/Downloads
+
 #try
 #equery l -I \*
