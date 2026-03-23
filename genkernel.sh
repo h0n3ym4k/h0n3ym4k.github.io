8d5d6110ccd90e98e90e7fbfc43726fa12e70e51
h0n3ym4k <98036119lmak@gmail.com>
2026-03-23 17:50:49 +0800

    Update genkernel.sh

--- genkernel.sh
+++ genkernel.sh
@@ -1,1 +1,2 @@
-genkernel all --mountboot --bootloader=grub --firmware --microcode --virtio --lvm --postclear --oldconfig
+genkernel all --loglevel=5 --color --mountboot --bootloader=grub --firmware --microcode --virtio --lvm --postclear --oldconfig
+echo ''|mailx -s 'kernel compile complete' root
